307605e647caffac6edf7b243b0c88968e67e7f8
Dario Eugenio <eugeniodario6@gmail.com>
2026-08-22 17:34:09 +0200

    fix: align HealthData interface with page.tsx field names (heartRate, activeCalories, workouts)

--- lib/healthkit.ts
+++ lib/healthkit.ts
@@ -1,5 +1,12 @@
 import { registerPlugin } from '@capacitor/core';
 
+export interface HKWorkout {
+  type: string;
+  duration: number;   // minutes
+  calories: number;   // kcal
+  date: string;       // ISO8601
+}
+
 export interface HealthKitPlugin {
   requestPermissions(): Promise<{ granted: boolean }>;
   getData(): Promise<HealthData>;
@@ -8,12 +15,15 @@ export interface HealthKitPlugin {
 export interface HealthData {
   available: boolean;
   steps: number;
+  heartRate: number;
   bpm: number;
   hrv: number;
   sleepHours: number;
   sleepMinutes: number;
   weight: number;
+  activeCalories: number;
   kcal: number;
+  workouts: HKWorkout[];
 }
 
 const HealthKit = registerPlugin<HealthKitPlugin>('HealthKit', {
@@ -21,9 +31,9 @@ const HealthKit = registerPlugin<HealthKitPlugin>('HealthKit', {
     requestPermissions: async () => ({ granted: false }),
     getData: async () => ({
       available: false,
-      steps: 0, bpm: 0, hrv: 0,
+      steps: 0, heartRate: 0, bpm: 0, hrv: 0,
       sleepHours: 0, sleepMinutes: 0,
-      weight: 0, kcal: 0,
+      weight: 0, activeCalories: 0, kcal: 0, workouts: [],
     }),
   },
 });
@@ -39,6 +49,6 @@ export async function getHealthData(): Promise<HealthData> {
   try {
     return await HealthKit.getData();
   } catch {
-    return { available: false, steps: 0, bpm: 0, hrv: 0, sleepHours: 0, sleepMinutes: 0, weight: 0, kcal: 0 };
+    return { available: false, steps: 0, heartRate: 0, bpm: 0, hrv: 0, sleepHours: 0, sleepMinutes: 0, weight: 0, activeCalories: 0, kcal: 0, workouts: [] };
   }
 }
